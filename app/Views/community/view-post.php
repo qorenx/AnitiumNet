86b5a3a21fd70244e7f60bc0934e32e293e86fbf
Çağatay İlhan <icagatay930@gmail.com>
2023-06-29 14:08:36 +0300

    post and postmore fix
    function boardviewpost  and function boardviewpostmore fixed

--- app/Views/community/view-post.php
+++ app/Views/community/view-post.php
@@ -10,44 +10,34 @@
                 </div>
                 <?= $this->include('community/boardextend/boardpostview') ?>
                 <div class="list-comment">
-                    <div class="cw_list">
-                        <?php
-                        $url = base_url("community/bpviewlist?pid={$page['pid']}&page={$page['currentPage']}");
-                        $data = file_get_contents($url);
-                        $htmlData = json_decode($data, true)['html'];
-                        echo $htmlData;
-                        ?>
-                        <div id="result"></div>
-
-                        <?php if ($page['status'] !== false) : ?>
-                            <button style="margin: 0 auto; display: block; font-size: 16px; padding: 10px 20px; background-color: #353b4d; color: #fff; border: none; border-radius: 5px; cursor: pointer;" onclick="runCommand()" id="viewpostbutton">Show More</button>
-                        <?php endif; ?>
-                        <script>
-                            var nextPage = <?php echo $page['nextPage']; ?>;
-
-                            function runCommand() {
-                                var xhr = new XMLHttpRequest();
-                                xhr.open("GET", "/community/bpviewlist?pid=<?php echo $page['pid']; ?>&page=" + nextPage, true);
-                                xhr.onreadystatechange = function() {
-                                    if (xhr.readyState === 4 && xhr.status === 200) {
-                                        var data = JSON.parse(xhr.responseText);
-                                        var htmlData = data['html'];
-                                        var htmlData2 = data['page']['status'];
-                                        var resultElement = document.getElementById("result");
-                                        resultElement.innerHTML += htmlData;
-
-                                        nextPage++;
-
-                                        if (htmlData2 === false) {
-                                            var buttonElement = document.getElementById("viewpostbutton");
-                                            buttonElement.style.display = "none";
-                                        }
-                                    }
-                                };
-                                xhr.send();
-                            }
-                        </script>
+                    <div id="result" class="cw_list">
                     </div>
+                        <button style="margin: 0 auto; display: block; font-size: 16px; padding: 10px 20px; background-color: #353b4d; color: #fff; border: none; border-radius: 5px; cursor: pointer;" onclick="runCommand()" id="viewpostbutton">Show More</button>
+                    <script>
+                        var nextPage = 1;
+
+                        function runCommand() {
+                            var xhr = new XMLHttpRequest();
+                            xhr.open("GET", "/community/bpviewlist?pid=<?php $pid = $_GET['pid'];echo $pid;?>&page=" + nextPage, true);
+                            xhr.onreadystatechange = function() {
+                                if (xhr.readyState === 4 && xhr.status === 200) {
+                                    var data = JSON.parse(xhr.responseText);
+                                    var htmlData = data['html'];
+                                    var htmlData2 = data['page']['status'];
+                                    var resultElement = document.getElementById("result");
+                                    resultElement.innerHTML += htmlData;
+
+                                    nextPage++;
+
+                                    if (htmlData2 === false) {
+                                        var buttonElement = document.getElementById("viewpostbutton");
+                                        buttonElement.style.display = "none";
+                                    }
+                                }
+                            };
+                            xhr.send();
+                        }
+                    </script>
                 </div>
             </div>
         </div>
